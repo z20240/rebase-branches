5ff03a89371eecb8fbbcbb3590595f8a12a5a162
les <les.lee@slotfactory.com>
2020-02-18 17:09:42 +0800

    new verb

--- index.js
+++ index.js
@@ -2,19 +2,38 @@
 
 const { exec } = require('child_process');
 const { promisify } = require('util');
+const chalk = require('chalk');
 
 const main = async() => {
-    const { stdout, stderr } = await promisify(exec)('git branch');
-    stdout
-        .split('\n')
-        .map(line => line.replace('* ', '').trim())
-        .filter(line => !!line)
-        .forEach(async(br_name) => {
-            const { stdout, stderr } = await promisify(exec)(`git rebase --onto master ${ br_name }`);
+    const async_exec = promisify(exec);
+    try {
+        var { stdout } = await async_exec('git status');
 
-            console.log(`【${ br_name }】 --> rebase master \n`, stdout);
+        const curr_br_name = stdout.split('\n')[0].trim().match(/On branch (.+)/)[1];
+
+        var { stdout } = await async_exec('git branch');
+        const br_names = stdout
+            .split('\n')
+            .map(line => line.replace('* ', '').trim())
+            .filter(line => !!line && line !== 'master');
+
+        for (const br_name of br_names) {
+            var { stdout } = await async_exec(`git checkout ${ br_name }`);
+
+            console.log(chalk.blue(`【${ br_name }】 --> checkout to ${ br_name } \n`), stdout);
+
+            var { stdout } = await async_exec(`git rebase master`);
+
+            console.log(chalk.green(`【${ br_name }】 --> rebase master completed \n`), stdout);
             console.log('---------\n\n');
-        });
+        }
+
+        await async_exec(`git checkout ${ curr_br_name }`);
+
+        console.log(chalk.cyan.bgWhite('~~~ All Completed ~~~'));
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 main();
